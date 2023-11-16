075b021ec4eba24a3d69a97c483d888d2b308d09
iliakior <ilias.kioroglou@satorianalytics.com>
2023-11-16 18:11:20 +0200

    first linting fixes

--- src/model/train.py
+++ src/model/train.py
@@ -2,7 +2,6 @@
 import argparse
 import glob
 import os
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -21,7 +20,7 @@ def main(args):
     X_train, X_test, y_train, y_test = split_data(df)
 
     # train model
-    model = train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
     # end MLflow run
     mlflow.end_run()
